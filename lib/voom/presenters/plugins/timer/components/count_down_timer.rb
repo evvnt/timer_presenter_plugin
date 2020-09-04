6c90aee67679fedba7f4d7206e002ab0748e0d19
Derek Graham <derek@evvnt.com>
2020-09-03 23:09:55 -0400

    WIP: Fire events on a schedule

--- lib/voom/presenters/plugins/timer/components/count_down_timer.rb
+++ lib/voom/presenters/plugins/timer/components/count_down_timer.rb
@@ -6,10 +6,11 @@ module Voom
       module Timer
         module Components
           class CountDownTimer < DSL::Components::EventBase
-            attr_reader :end_time, :expired_message
+            attr_reader :end_time, :schedule, :expired_message
 
             def initialize(end_time, **attribs, &block)
               @end_time = end_time
+              @schedule = attribs.delete(:schedule) { {} }
               @expired_message = attribs.delete(:expired_message) { 'EXPIRED' }
               super(type: :count_down_timer, **attribs, &block)
               expand!
